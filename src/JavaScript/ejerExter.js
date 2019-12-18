327b2e2e8ff0d8783970d6ed0cc276f012a56429
Antz Escorpion <escorpion2005@gmail.com>
2019-12-18 11:38:44 -0600

    script 2

--- src/JavaScript/ejerExter.js
+++ src/JavaScript/ejerExter.js
@@ -1,20 +1,26 @@
 //java Script
-var a=0;
-function ejecuta(){
+var a = 1;
+
+function ejecuta() {
     // document.getElementsByTagName("p")[0].onclick=alerta;  
     // document.getElementById("sec").onclick=alerta;
     // for (var i=0; i<2;i++){
-        
+
     //     document.getElementsByTagName("p")[i].onclick=alerta;
-        
+
     // }
-    
-    var x= document.getElementsByClassName("secu")[0].onclick=alerta;
-    
+
+
+    for (var i = 0; i < 2; i++) {
+
+        var x = document.getElementsByClassName("secu")[i].onclick = alerta;
+        // a = document.getElementsByClassName("secu")[i].
+    }
+
 }
 
-function alerta(){
-    alert("Nuevo quien:"  );
+function alerta() {
+    alert("Nuevo quien:" + a);
 }
 
-window.onload=ejecuta;
+window.onload = ejecuta;
